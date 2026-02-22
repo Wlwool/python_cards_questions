32e6a795c94d8fc2e4ac89db5d984f12b4dcd95b
Wlwool <blackenedima@yandex.ru>
2026-02-22 17:59:58 +0300

    add model_config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,4 @@
+from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
 
@@ -6,9 +7,10 @@ class Settings(BaseSettings):
     secret_key: str = "5"
     database_url: str = "sqlite:///./data/cards.db"
     token_expire_hours: int = 24
+    model_config = ConfigDict(env_file="../.env", extra="ignore")
 
-    class Config:
-        env_file = "../.env"
-        extra = "ignore"
+    # class Config:
+    #     env_file = "../.env"
+    #     extra = "ignore"
 
 settings = Settings()
